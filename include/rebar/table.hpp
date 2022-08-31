7f892c265771a10ffb9b2608e79613c94c57c648
Max Goddard <buckell@alternus.dev>
2022-08-31 00:36:03 -0700

    Fixed indenting.

--- include/rebar/table.hpp
+++ include/rebar/table.hpp
@@ -20,86 +20,85 @@ namespace rebar {
             std::allocator<std::pair<object, object>>,
             typename std::allocator_traits<std::allocator<std::pair<object, object>>>::template rebind_alloc<ska::detailv3::sherwood_v3_entry<std::pair<object, object>>>
     > {
-    size_t m_reference_count = 0;
+        size_t m_reference_count = 0;
 
-    [[nodiscard]] inline object& operator[](const object a_key) {
-        return emplace(a_key, object()).first->second;
-    }
-
-    [[nodiscard]] object& at(const object a_key) {
-        auto found = this->find(a_key);
-
-        if (found == this->cend()) {
-            throw std::out_of_range("Argument passed to at() was not in the map.");
+        [[nodiscard]] inline object& operator[](const object a_key) {
+            return emplace(a_key, object()).first->second;
         }
 
-        return found->second;
-    }
+        [[nodiscard]] object& at(const object a_key) {
+            auto found = this->find(a_key);
 
-    [[nodiscard]] const object& at(const object a_key) const {
-        auto found = this->find(a_key);
+            if (found == this->cend()) {
+                throw std::out_of_range("Argument passed to at() was not in the map.");
+            }
 
-        if (found == this->cend()) {
-            throw std::out_of_range("Argument passed to at() was not in the map.");
+            return found->second;
         }
 
-        return found->second;
-    }
+        [[nodiscard]] const object& at(const object a_key) const {
+            auto found = this->find(a_key);
 
-    [[nodiscard]] object index(const object a_key) const {
-        auto found = this->find(a_key);
+            if (found == this->cend()) {
+                throw std::out_of_range("Argument passed to at() was not in the map.");
+            }
 
-        if (found == this->cend()) {
-            return {};
+            return found->second;
         }
 
-        return found->second;
-    }
+        [[nodiscard]] object index(const object a_key) const {
+            auto found = this->find(a_key);
 
-    void add(const table& a_table) {
-        for (const std::pair<object, object>& pair : a_table) {
-            emplace(pair);
-        }
-    }
+            if (found == this->cend()) {
+                return {};
+            }
 
-    public:
-
-    [[nodiscard]] friend bool operator==(const table& lhs, const table& rhs) {
-        if (lhs.size() != rhs.size()) {
-            return false;
+            return found->second;
         }
 
-        for (const std::pair<object, object>& value : lhs) {
-            auto found = rhs.find(value.first);
-
-            if (found == rhs.end() || value.second != found->second){
-                return false;
+        void add(const table& a_table) {
+            for (const std::pair<object, object>& pair : a_table) {
+                emplace(pair);
             }
         }
 
-        return true;
-    }
+    public:
+        [[nodiscard]] friend bool operator==(const table& lhs, const table& rhs) {
+            if (lhs.size() != rhs.size()) {
+                return false;
+            }
 
-    [[nodiscard]] friend bool operator!=(const table& lhs, const table& rhs) {
-        return !(lhs == rhs);
-    }
+            for (const std::pair<object, object>& value : lhs) {
+                auto found = rhs.find(value.first);
 
-    // TODO: Add "add" and "add assignment" operator.
+                if (found == rhs.end() || value.second != found->second){
+                    return false;
+                }
+            }
 
-    [[nodiscard]] friend object index(const table* a_table, const object& a_key) {
-        auto found = a_table->find(a_key);
-
-        if (found == a_table->end()) {
-            return object();
+            return true;
         }
 
-        return found->second;
-    }
+        [[nodiscard]] friend bool operator!=(const table& lhs, const table& rhs) {
+            return !(lhs == rhs);
+        }
 
-    friend void emplace(table* a_table, const object a_key, const object a_value) {
-        a_table->emplace(a_key, a_value);
-    }
-};
+        // TODO: Add "add" and "add assignment" operator.
+
+        [[nodiscard]] friend object index(const table* a_table, const object& a_key) {
+            auto found = a_table->find(a_key);
+
+            if (found == a_table->end()) {
+                return object();
+            }
+
+            return found->second;
+        }
+
+        friend void emplace(table* a_table, const object a_key, const object a_value) {
+            a_table->emplace(a_key, a_value);
+        }
+    };
 }
 
 #endif //REBAR_TABLE_HPP
