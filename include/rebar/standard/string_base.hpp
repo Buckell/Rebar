e09a8fba838fb40052c62530db6b5d7c24ae49eb
Max Goddard <buckell@alternus.dev>
2022-09-13 20:39:15 -0700

    Implemented all current string functions (except REGEX).

--- include/rebar/standard/string_base.hpp
+++ include/rebar/standard/string_base.hpp
@@ -14,7 +14,35 @@ namespace rebar::library::standard {
         string_base() : library(usage::implicit_include) {}
 
         static REBAR_FUNCTION(Contains) {
-            *ret = null;
+            std::string_view self = env->arg(0).get_string().to_string_view();
+            std::string_view compare = env->arg(1).get_string().to_string_view();
+
+            size_t self_size = self.size();
+            size_t compare_size = compare.size();
+
+            if (self_size < compare_size) {
+                REBAR_RETURN(boolean_false);
+            }
+
+            size_t compare_index = 0;
+
+            for (size_t i = 0; i <= self_size; ++i) {
+                if (self[i] == compare[compare_index]) {
+                    ++compare_index;
+
+                    if (compare_index >= compare_size) {
+                        REBAR_RETURN(boolean_true);
+                    }
+                } else {
+                    compare_index = 0;
+
+                    if (i > (self_size - compare_size)) {
+                        REBAR_RETURN(boolean_false);
+                    }
+                }
+            }
+
+            REBAR_RETURN(boolean_false);
         }
 
         static REBAR_FUNCTION(EndsWith) {
@@ -22,11 +50,10 @@ namespace rebar::library::standard {
             std::string_view compare = env->arg(1).get_string().to_string_view();
 
             if (compare.length() > self.length()) {
-                *ret = false;
-                return;
+                REBAR_RETURN(boolean_false);
             }
 
-            *ret = self.substr(self.length() - compare.length(), compare.length()) == compare;
+            REBAR_RETURN(object::from_bool(self.substr(self.length() - compare.length(), compare.length()) == compare));
         }
 
         static REBAR_FUNCTION(EqualsIgnoreCase) {
@@ -34,46 +61,229 @@ namespace rebar::library::standard {
             std::string_view compare = env->arg(1).get_string().to_string_view();
 
             if (self.length() != compare.length()) {
-                *ret = false;
-                return;
+                REBAR_RETURN(boolean_false);
             }
 
             for (size_t i = 0; i < self.length(); ++i) {
                 if (std::tolower(self[i]) != std::tolower(compare[i])) {
-                    *ret = false;
-                    return;
+                    REBAR_RETURN(boolean_false);
                 }
             }
 
-            *ret = true;
+            REBAR_RETURN(boolean_true);
         }
 
         static REBAR_FUNCTION(IndexOf) {
-            *ret = null;
+            std::string_view self = env->arg(0).get_string().to_string_view();
+            std::string_view compare = env->arg(1).get_string().to_string_view();
+
+            size_t self_size = self.size();
+            size_t compare_size = compare.size();
+
+            if (self_size < compare_size) {
+                REBAR_RETURN(null);
+            }
+
+            size_t start_index = 0;
+            size_t compare_index = 0;
+
+            for (size_t i = 0; i <= self_size; ++i) {
+                if (self[i] == compare[compare_index]) {
+                    if (compare_index == 0) {
+                        start_index = i;
+                    }
+
+                    ++compare_index;
+
+                    if (compare_index >= compare_size) {
+                        REBAR_RETURN(static_cast<integer>(start_index));
+                    }
+                } else {
+                    compare_index = 0;
+
+                    if (i > (self_size - compare_size)) {
+                        REBAR_RETURN(null);
+                    }
+                }
+            }
+
+            REBAR_RETURN(null);
         }
 
         static REBAR_FUNCTION(IsEmpty) {
-            *ret = null;
+            std::string_view self = env->arg(0).get_string().to_string_view();
+            REBAR_RETURN(object::from_bool(self.empty()));
         }
 
         static REBAR_FUNCTION(LastIndexOf) {
-            *ret = null;
+            std::string_view self = env->arg(0).get_string().to_string_view();
+            std::string_view compare = env->arg(1).get_string().to_string_view();
+
+            size_t self_size = self.size();
+            size_t compare_size = compare.size();
+
+            if (self_size < compare_size) {
+                REBAR_RETURN(null);
+            }
+
+            size_t compare_index = 0;
+
+            for (size_t i = self_size - 1; i > 0; --i) {
+                if (self[i] == compare[compare_size - compare_index - 1]) {
+                    ++compare_index;
+
+                    if (compare_index >= compare_size) {
+                        REBAR_RETURN(static_cast<integer>(i));
+                    }
+                } else {
+                    compare_index = 0;
+
+                    if (i < (self_size - compare_size)) {
+                        REBAR_RETURN(null);
+                    }
+                }
+            }
+
+            REBAR_RETURN(null);
         }
 
         static REBAR_FUNCTION(Length) {
-            *ret = static_cast<integer>(env->arg(0).get_string().length());
+            REBAR_RETURN(static_cast<integer>(env->arg(0).get_string().length()));
         }
 
         static REBAR_FUNCTION(Matches) {
-            *ret = null;
+            // TODO: Implement REGEX.
+            REBAR_RETURN(null);
         }
 
         static REBAR_FUNCTION(Replace) {
-            *ret = null;
+            std::string_view self = env->arg(0).get_string().to_string_view();
+            std::string_view search = env->arg(1).get_string().to_string_view();
+            std::string_view replace = env->arg(2).get_string().to_string_view();
+
+            size_t self_size = self.size();
+            size_t search_size = search.size();
+            size_t replace_size = replace.size();
+
+            size_t start_index = self_size + 1;
+            size_t compare_index = 0;
+
+            if (search_size == replace_size) {
+                std::string buffer{ self };
+
+                for (size_t i = 0; i <= self_size; ++i) {
+                    if (self[i] == search[compare_index]) {
+                        if (compare_index == 0) {
+                            start_index = i;
+                        }
+
+                        ++compare_index;
+
+                        if (compare_index >= search_size) {
+                            compare_index = 0;
+                            memcpy(buffer.data() + start_index, replace.data(), replace_size);
+                        }
+                    } else {
+                        compare_index = 0;
+
+                        if (i > (self_size - search_size)) {
+                            REBAR_RETURN(env->str(buffer));
+                        }
+                    }
+                }
+            } else {
+                std::string buffer;
+                buffer.reserve(self_size);
+
+                for (size_t i = 0; i <= self_size; ++i) {
+                    if (self[i] == search[compare_index]) {
+                        ++compare_index;
+
+                        if (compare_index >= search_size) {
+                            compare_index = 0;
+                            buffer += replace;
+                        }
+                    } else {
+                        compare_index = 0;
+
+                        if (i > (self_size - search_size)) {
+                            buffer += self.substr(i);
+                            REBAR_RETURN(env->str(buffer));
+                        }
+
+                        buffer += self[i];
+                    }
+                }
+
+                REBAR_RETURN(env->str(buffer));
+            }
         }
 
         static REBAR_FUNCTION(Split) {
-            *ret = null;
+            std::string_view self = env->arg(0).get_string().to_string_view();
+            std::string_view delimiter = env->arg(1).get_string().to_string_view();
+
+            size_t self_size = self.size();
+            size_t delimiter_size = delimiter.size();
+
+            array arr(4);
+
+            size_t begin_index = 0;
+
+            if (delimiter_size == 1) {
+                char delimiter_character = delimiter[0];
+
+                for (size_t i = 0; i < self_size; ++i) {
+                    char c = self[i];
+
+                    if (c == delimiter_character) {
+                        if (i - begin_index > 0) {
+                            arr.push_back(env->str(self.substr(begin_index, i - begin_index)));
+                        }
+
+                        begin_index = i + 1;
+                    }
+                }
+
+                if (begin_index != self_size - 1) {
+                    std::string_view str = self.substr(begin_index);
+
+                    if (!str.empty()) {
+                        arr.push_back(env->str(self.substr(begin_index)));
+                    }
+                }
+            } else {
+                size_t compare_index = 0;
+
+                for (size_t i = 0; i < self_size; ++i) {
+                    if (self[i] == delimiter[compare_index]) {
+                        ++compare_index;
+
+                        if (compare_index >= delimiter_size) {
+                            compare_index = 0;
+
+                            if (i - begin_index - delimiter_size + 1 > 0) {
+                                arr.push_back(env->str(self.substr(begin_index, i - begin_index - delimiter_size + 1)));
+                            }
+
+                            begin_index = i + 1;
+                        }
+                    } else {
+                        compare_index = 0;
+
+                        if (i > (self_size - delimiter_size)) {
+                            break;
+                        }
+                    }
+                }
+
+                std::string_view str = self.substr(begin_index);
+                if (!str.empty()) {
+                    arr.push_back(env->str(str));
+                }
+            }
+
+            REBAR_RETURN(arr);
         }
 
         static REBAR_FUNCTION(StartsWith) {
@@ -81,14 +291,22 @@ namespace rebar::library::standard {
             std::string_view compare = env->arg(1).get_string().to_string_view();
 
             if (compare.length() > self.length()) {
-                *ret = false;
+                REBAR_RETURN(boolean_false);
             }
 
-            *ret = self.substr(0, compare.length()) == compare;
+            REBAR_RETURN(object::from_bool(self.substr(0, compare.length()) == compare));
         }
 
         static REBAR_FUNCTION(ToCharArray) {
-            *ret = null;
+            std::string_view self = env->arg(0).get_string().to_string_view();
+
+            array arr(self.size());
+
+            for (char c : self) {
+                arr.push_back(static_cast<integer>(c));
+            }
+
+            REBAR_RETURN(arr);
         }
 
         static REBAR_FUNCTION(ToLowerCase) {
@@ -97,7 +315,7 @@ namespace rebar::library::standard {
             std::string output;
             output.reserve(self.length());
 
-            std::transform(self.begin(), self.end(), output.begin(), static_cast<int(*)(int)>(std::tolower));
+            std::transform(self.begin(), self.end(), std::back_inserter(output), static_cast<int(*)(int)>(std::tolower));
 
             *ret = env->str(output);
         }
@@ -108,7 +326,7 @@ namespace rebar::library::standard {
             std::string output;
             output.reserve(self.length());
 
-            std::transform(self.begin(), self.end(), output.begin(), static_cast<int(*)(int)>(std::tolower));
+            std::transform(self.begin(), self.end(), std::back_inserter(output), static_cast<int(*)(int)>(std::toupper));
 
             *ret = env->str(output);
         }
