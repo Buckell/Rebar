e1398babd88edfe92939237cbd1b7da29af2261f
Max Goddard <buckell@alternus.dev>
2022-10-03 09:29:33 -0700

    Changed native object operator overloads to use object references.

--- include/rebar/native_object_impl.hpp
+++ include/rebar/native_object_impl.hpp
@@ -14,28 +14,28 @@ namespace rebar {
     struct virtual_table : public table {
         template <size_t v_arguments>
         using assignment_operation_function = std::conditional_t<v_arguments == 0, void (*)(environment*, native_object),
-        std::conditional_t<v_arguments == 1, void (*)(environment*, native_object, object),
-                std::conditional_t<v_arguments == 2, void (*)(environment*, native_object, object, object),
+        std::conditional_t<v_arguments == 1, void (*)(environment*, native_object, object&),
+                std::conditional_t<v_arguments == 2, void (*)(environment*, native_object, object&, object&),
                 void>>>;
 
         template <size_t v_arguments>
         using operation_function = std::conditional_t<v_arguments == 0, object (*)(environment*, native_object),
-        std::conditional_t<v_arguments == 1, object (*)(environment*, native_object, object),
-                std::conditional_t<v_arguments == 2, object (*)(environment*, native_object, object, object),
+        std::conditional_t<v_arguments == 1, object (*)(environment*, native_object, object&),
+                std::conditional_t<v_arguments == 2, object (*)(environment*, native_object, object&, object&),
                 void>>>;
 
-        using index_function = object& (*)(environment*, native_object, object);
+        using index_function = object& (*)(environment*, native_object, object&);
 
         // TODO: Throw exceptions in null functions.
         constexpr static const assignment_operation_function<0> null_aof_0 = [](environment*, native_object) {};
-        constexpr static const assignment_operation_function<1> null_aof_1 = [](environment*, native_object, object) {};
-        constexpr static const assignment_operation_function<2> null_aof_2 = [](environment*, native_object, object, object) {};
+        constexpr static const assignment_operation_function<1> null_aof_1 = [](environment*, native_object, object&) {};
+        constexpr static const assignment_operation_function<2> null_aof_2 = [](environment*, native_object, object&, object&) {};
 
         constexpr static const operation_function<0> null_of_0 = [](environment*, native_object) -> object { return null; };
-        constexpr static const operation_function<1> null_of_1 = [](environment*, native_object, object) -> object { return null; };
-        constexpr static const operation_function<2> null_of_2 = [](environment*, native_object, object, object) -> object { return null; };
+        constexpr static const operation_function<1> null_of_1 = [](environment*, native_object, object&) -> object { return null; };
+        constexpr static const operation_function<2> null_of_2 = [](environment*, native_object, object&, object&) -> object { return null; };
 
-        constexpr static const index_function null_idx = [](environment*, native_object, object) -> object& { static object temp; return temp; };
+        constexpr static const index_function null_idx = [](environment*, native_object, object&) -> object& { static object temp; return temp; };
 
         assignment_operation_function<1> overload_assignment                  = null_aof_1;
         operation_function<1>            overload_addition                    = null_of_1;
@@ -84,71 +84,71 @@ namespace rebar {
 
     // NATIVE OBJECT OVERLOAD FUNCTIONS
 
-    inline void native_object::overload_assignment(environment& a_env, const object rhs) {
+    inline void native_object::overload_assignment(environment& a_env, object& rhs) {
         this->get_virtual_table().overload_assignment(&a_env, *this, rhs);
     }
 
-    [[nodiscard]] inline object native_object::overload_addition(environment& a_env, const object rhs) {
+    [[nodiscard]] inline object native_object::overload_addition(environment& a_env, object& rhs) {
         return this->get_virtual_table().overload_addition(&a_env, *this, rhs);
     }
 
-    inline void native_object::overload_addition_assignment(environment& a_env, const object rhs) {
+    inline void native_object::overload_addition_assignment(environment& a_env, object& rhs) {
         this->get_virtual_table().overload_addition_assignment(&a_env, *this, rhs);
     }
 
-    [[nodiscard]] inline object native_object::overload_multiplication(environment& a_env, const object rhs) {
+    [[nodiscard]] inline object native_object::overload_multiplication(environment& a_env, object& rhs) {
         return this->get_virtual_table().overload_multiplication(&a_env, *this, rhs);
     }
 
-    inline void native_object::overload_multiplication_assignment(environment& a_env, const object rhs) {
+    inline void native_object::overload_multiplication_assignment(environment& a_env, object& rhs) {
         this->get_virtual_table().overload_multiplication_assignment(&a_env, *this, rhs);
     }
 
-    [[nodiscard]] inline object native_object::overload_division(environment& a_env, const object rhs) {
+    [[nodiscard]] inline object native_object::overload_division(environment& a_env, object& rhs) {
         return this->get_virtual_table().overload_division(&a_env, *this, rhs);
     }
 
-    inline void native_object::overload_division_assignment(environment& a_env, const object rhs) {
+    inline void native_object::overload_division_assignment(environment& a_env, object& rhs) {
         this->get_virtual_table().overload_division_assignment(&a_env, *this, rhs);
     }
 
-    [[nodiscard]] inline object native_object::overload_subtraction(environment& a_env, const object rhs) {
+    [[nodiscard]] inline object native_object::overload_subtraction(environment& a_env, object& rhs) {
         return this->get_virtual_table().overload_subtraction(&a_env, *this, rhs);
     }
 
-    inline void native_object::overload_subtraction_assignment(environment& a_env, const object rhs) {
+    inline void native_object::overload_subtraction_assignment(environment& a_env, object& rhs) {
         this->get_virtual_table().overload_subtraction_assignment(&a_env, *this, rhs);
     }
 
-    [[nodiscard]] inline object native_object::overload_equality(environment& a_env, const object rhs) {
+    [[nodiscard]] inline object native_object::overload_equality(environment& a_env, object& rhs) {
         return this->get_virtual_table().overload_equality(&a_env, *this, rhs);
     }
 
-    [[nodiscard]] inline object native_object::overload_inverse_equality(environment& a_env, const object rhs) {
+    [[nodiscard]] inline object native_object::overload_inverse_equality(environment& a_env, object& rhs) {
         return this->get_virtual_table().overload_inverse_equality(&a_env, *this, rhs);
     }
 
-    [[nodiscard]] inline object native_object::overload_greater(environment& a_env, const object rhs) {
+    [[nodiscard]] inline object native_object::overload_greater(environment& a_env, object& rhs) {
         return this->get_virtual_table().overload_greater(&a_env, *this, rhs);
     }
 
-    [[nodiscard]] inline object native_object::overload_lesser(environment& a_env, const object rhs) {
+    [[nodiscard]] inline object native_object::overload_lesser(environment& a_env, object& rhs) {
         return this->get_virtual_table().overload_lesser(&a_env, *this, rhs);
     }
 
-    [[nodiscard]] inline object native_object::overload_greater_equality(environment& a_env, const object rhs) {
+    [[nodiscard]] inline object native_object::overload_greater_equality(environment& a_env, object& rhs) {
         return this->get_virtual_table().overload_greater_equality(&a_env, *this, rhs);
     }
 
-    [[nodiscard]] inline object native_object::overload_lesser_equality(environment& a_env, const object rhs) {
+    [[nodiscard]] inline object native_object::overload_lesser_equality(environment& a_env, object& rhs) {
         return this->get_virtual_table().overload_lesser_equality(&a_env, *this, rhs);
     }
 
-    [[nodiscard]] inline object native_object::overload_logical_or(environment& a_env, const object rhs) {
+    [[nodiscard]] inline object native_object::overload_logical_or(environment& a_env, object& rhs) {
         return this->get_virtual_table().overload_logical_or(&a_env, *this, rhs);
     }
 
-    [[nodiscard]] inline object native_object::overload_logical_and(environment& a_env, const object rhs) {
+    [[nodiscard]] inline object native_object::overload_logical_and(environment& a_env, object& rhs) {
         return this->get_virtual_table().overload_logical_and(&a_env, *this, rhs);
     }
 
@@ -156,27 +156,27 @@ namespace rebar {
         return this->get_virtual_table().overload_logical_not(&a_env, *this);
     }
 
-    [[nodiscard]] inline object native_object::overload_bitwise_or(environment& a_env, const object rhs) {
+    [[nodiscard]] inline object native_object::overload_bitwise_or(environment& a_env, object& rhs) {
         return this->get_virtual_table().overload_bitwise_or(&a_env, *this, rhs);
     }
 
-    inline void native_object::overload_bitwise_or_assignment(environment& a_env, const object rhs) {
+    inline void native_object::overload_bitwise_or_assignment(environment& a_env, object& rhs) {
         this->get_virtual_table().overload_bitwise_or_assignment(&a_env, *this, rhs);
     }
 
-    [[nodiscard]] inline object native_object::overload_bitwise_xor(environment& a_env, const object rhs) {
+    [[nodiscard]] inline object native_object::overload_bitwise_xor(environment& a_env, object& rhs) {
         return this->get_virtual_table().overload_bitwise_xor(&a_env, *this, rhs);
     }
 
-    inline void native_object::overload_bitwise_xor_assignment(environment& a_env, const object rhs) {
+    inline void native_object::overload_bitwise_xor_assignment(environment& a_env, object& rhs) {
         this->get_virtual_table().overload_bitwise_xor_assignment(&a_env, *this, rhs);
     }
 
-    [[nodiscard]] inline object native_object::overload_bitwise_and(environment& a_env, const object rhs) {
+    [[nodiscard]] inline object native_object::overload_bitwise_and(environment& a_env, object& rhs) {
         return this->get_virtual_table().overload_bitwise_and(&a_env, *this, rhs);
     }
 
-    inline void native_object::overload_bitwise_and_assignment(environment& a_env, const object rhs) {
+    inline void native_object::overload_bitwise_and_assignment(environment& a_env, object& rhs) {
         this->get_virtual_table().overload_bitwise_and_assignment(&a_env, *this, rhs);
     }
 
@@ -184,35 +184,35 @@ namespace rebar {
         return this->get_virtual_table().overload_bitwise_not(&a_env, *this);
     }
 
-    [[nodiscard]] inline object native_object::overload_shift_right(environment& a_env, const object rhs) {
+    [[nodiscard]] inline object native_object::overload_shift_right(environment& a_env, object& rhs) {
         return this->get_virtual_table().overload_shift_right(&a_env, *this, rhs);
     }
 
-    inline void native_object::overload_shift_right_assignment(environment& a_env, const object rhs) {
+    inline void native_object::overload_shift_right_assignment(environment& a_env, object& rhs) {
         this->get_virtual_table().overload_shift_right_assignment(&a_env, *this, rhs);
     }
 
-    [[nodiscard]] inline object native_object::overload_shift_left(environment& a_env, const object rhs) {
+    [[nodiscard]] inline object native_object::overload_shift_left(environment& a_env, object& rhs) {
         return this->get_virtual_table().overload_shift_left(&a_env, *this, rhs);
     }
 
-    inline void native_object::overload_shift_left_assignment(environment& a_env, const object rhs) {
+    inline void native_object::overload_shift_left_assignment(environment& a_env, object& rhs) {
         this->get_virtual_table().overload_shift_left_assignment(&a_env, *this, rhs);
     }
 
-    [[nodiscard]] inline object native_object::overload_exponent(environment& a_env, const object rhs) {
+    [[nodiscard]] inline object native_object::overload_exponent(environment& a_env, object& rhs) {
         return this->get_virtual_table().overload_exponent(&a_env, *this, rhs);
     }
 
-    inline void native_object::overload_exponent_assignment(environment& a_env, const object rhs) {
+    inline void native_object::overload_exponent_assignment(environment& a_env, object& rhs) {
         this->get_virtual_table().overload_exponent_assignment(&a_env, *this, rhs);
     }
 
-    [[nodiscard]] inline object native_object::overload_modulus(environment& a_env, const object rhs) {
+    [[nodiscard]] inline object native_object::overload_modulus(environment& a_env, object& rhs) {
         return this->get_virtual_table().overload_modulus(&a_env, *this, rhs);
     }
 
-    inline void native_object::overload_modulus_assignment(environment& a_env, const object rhs) {
+    inline void native_object::overload_modulus_assignment(environment& a_env, object& rhs) {
         this->get_virtual_table().overload_modulus_assignment(&a_env, *this, rhs);
     }
 
@@ -236,15 +236,15 @@ namespace rebar {
         return this->get_virtual_table().overload_operation_postfix_decrement(&a_env, *this);
     }
 
-    [[nodiscard]] inline object& native_object::overload_index(environment& a_env, const object rhs) {
+    [[nodiscard]] inline object& native_object::overload_index(environment& a_env, object& rhs) {
         return this->get_virtual_table().overload_operation_index(&a_env, *this, rhs);
     }
 
-    [[nodiscard]] inline object native_object::overload_select(environment& a_env, const object rhs) {
+    [[nodiscard]] inline object native_object::overload_select(environment& a_env, object& rhs) {
         return this->get_virtual_table().overload_operation_select(&a_env, *this, rhs);
     }
 
-    [[nodiscard]] inline object native_object::overload_ranged_select(environment& a_env, const object lhs, const object rhs) {
+    [[nodiscard]] inline object native_object::overload_ranged_select(environment& a_env, object& lhs, object& rhs) {
         return this->get_virtual_table().overload_operation_ranged_select(&a_env, *this, lhs, rhs);
     }
 
