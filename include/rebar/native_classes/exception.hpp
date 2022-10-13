a6b56c8c967f4d9154cd2b386f62f72a0a2fbb66
Max Goddard <buckell@alternus.dev>
2022-10-12 18:04:06 -0700

    Added additional functions to exception class to access individual stack trace entries.

--- include/rebar/native_classes/exception.hpp
+++ include/rebar/native_classes/exception.hpp
@@ -29,6 +29,38 @@ namespace rebar::native {
             env->cerr() << env->generate_exception_message() << std::endl;
             REBAR_RETURN(null);
         }
+
+        REBAR_NATIVE_FUNCTION(GetTraceCount) {
+            REBAR_RETURN(static_cast<integer>(env->get_stack_trace().entries().size()));
+        }
+
+        REBAR_NATIVE_FUNCTION(GetTraceRow) {
+            table row_table(*env);
+
+            size_t index = static_cast<size_t>(env->arg(1).get_integer());
+
+            auto& entry = env->get_stack_trace().entries().at(index);
+
+            function func = entry.get_function();
+            function_info& func_info = env->get_function_info(func);
+
+            row_table["function_name"] = env->str(func_info.m_name);
+
+            auto& orig = func_info.get_origin();
+
+            auto file_find_it = orig.info.find("FILE");
+            if (file_find_it != orig.info.cend()) {
+                row_table["file"] = env->str(file_find_it->second);
+            }
+
+            auto pos = entry.get_position();
+
+            row_table["source_index"] = static_cast<integer>(pos.first);
+            row_table["source_row"] = static_cast<integer>(pos.second.row);
+            row_table["source_column"] = static_cast<integer>(pos.second.column);
+
+            REBAR_RETURN(row_table);
+        }
     };
 
     object load_exception(environment& a_env) {
